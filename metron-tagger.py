f6ccc7267a318ade91815227b474ccfcd19a22ae
Brian Pepple <bdpepple@gmail.com>
2019-01-12 15:37:24 -0500

    Add options to get file list of zipfile (cbz)

--- metron-tagger.py
+++ metron-tagger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import os
 import sys
-from taggerlib.settings import MetronTaggerSettings
 from taggerlib.options import make_parser
+from taggerlib.settings import MetronTaggerSettings
 
 # Load the settings
 SETTINGS = MetronTaggerSettings()
@@ -22,9 +23,31 @@ def main():
     if opts.set_metron_user:
         SETTINGS.save()
 
-    if opt.recursive:
-        # TODO: get the file list of comic archives
-        pass
+    # Parse paths to get file list
+    full_paths = [os.path.join(os.getcwd(), path) for path in opts.path]
+    file_list = []
+
+    for path in full_paths:
+        if os.path.isfile(path):
+            fileName, fileExt = os.path.splitext(path)
+            # TODO: Use ZipFile to determine file type
+            if fileExt == '.cbz':
+                file_list.append(path)
+        else:
+            if opts.recursive:
+                if os.path.isdir(path):
+                    for root, dirs, files in os.walk(path):
+                        for f in files:
+                            fileName, fileExt = os.path.splitext(f)
+                            # TODO: Use ZipFile to determine file type
+                            if fileExt == '.cbz':
+                                file_list.append(os.path.join(root, f))
+
+    print(f'Files: {file_list}')
+
+    if not file_list:
+        print('No files to process. Exiting.')
+        sys.exit(0)
 
 
 if __name__ == '__main__':
--- taggerlib/options.py
+++ taggerlib/options.py
@@ -2,10 +2,13 @@ import argparse
 
 
 def make_parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Read in a file or set of files, and return the result.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('path', nargs='+', help='Path of a file or a folder of files.')
     parser.add_argument("-R", "--recursive",
                         help="Recursively include files in sub-folders",
-                        action="store_true")
+                        action="store_true",
+                        default=False)
     parser.add_argument("-u", "--user", help="Metron user identity")
     parser.add_argument("-p", "--password", help="Metron user identity")
     parser.add_argument("--set-metron-user",
