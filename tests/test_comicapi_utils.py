022680df8f3709db5ecb7920f478b1e9532995a6
Brian Pepple <bdpepple@gmail.com>
2019-05-07 17:27:57 -0400

    Add some tests for the comicapi utils

---
+++ tests/test_comicapi_utils.py
@@ -0,0 +1,22 @@
+import unittest
+
+from comicapi.utils import removearticles, listToString
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_remove_articles(self):
+        txt = 'The Champions & Inhumans'
+        new_txt = removearticles(txt)
+        self.assertEqual(new_txt, 'champions inhumans')
+
+    def test_list_to_string(self):
+        thislist = ["apple", "banana", "cherry"]
+        expected_result = "apple, banana, cherry"
+
+        list_string = listToString(thislist)
+        self.assertEqual(list_string, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
