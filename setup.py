db89320ddb7a2185206c5160d44a55178047f003
Brian Pepple <bdpepple@gmail.com>
2019-05-23 14:54:26 -0400

    Require python-3.7 or greater

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     url="https://github.com/bpepple/metron-tagger",
     license="GPLv3",
     packages=find_packages(),
+    python_requires=">=3.7",
     install_requires=["configparser", "natsort", "pillow"],
     entry_points={"console_scripts": ["metron-tagger=metrontagger.metron_tagger:main"]},
     classifiers=[
