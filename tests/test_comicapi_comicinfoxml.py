29ca3d81787fcf9057cddbaa5b327707de3f690c
Brian Pepple <bdpepple@gmail.com>
2019-05-28 15:21:37 -0400

    Increase the coverage for comicapi comicinfoxml

--- tests/test_comicapi_comicinfoxml.py
+++ tests/test_comicapi_comicinfoxml.py
@@ -33,3 +33,18 @@ class TestComicInfoXml(unittest.TestCase):
         res = open(tmp_file.name).read()
         tmp_file.close()
         self.assertIsNotNone(res)
+
+    def test_read_from_file(self):
+        tmp_file = tempfile.NamedTemporaryFile(suffix=".xml")
+        # Write metadata to file
+        ComicInfoXml().writeToExternalFile(tmp_file.name, self.md)
+        # Read the metadat from the file
+        new_md = ComicInfoXml().readFromExternalFile(tmp_file.name)
+        tmp_file.close()
+
+        self.assertIsNotNone(new_md)
+        self.assertEqual(new_md.series, self.md.series)
+        self.assertEqual(new_md.issue, self.md.issue)
+        self.assertEqual(new_md.year, self.md.year)
+        self.assertEqual(new_md.month, self.md.month)
+        self.assertEqual(new_md.day, self.md.day)
