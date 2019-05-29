61249b45f021d48b99ee4b4fcedce98eed1f66df
Brian Pepple <bdpepple@gmail.com>
2019-05-28 23:58:00 -0400

    Remove dependency on configparser from setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     license="GPLv3",
     packages=find_packages(),
     python_requires=">=3.7",
-    install_requires=["configparser", "natsort", "pillow", "ratelimit"],
+    install_requires=["natsort", "pillow", "ratelimit"],
     entry_points={"console_scripts": ["metron-tagger=metrontagger.metron_tagger:main"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
