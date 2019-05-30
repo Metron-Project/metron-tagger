d081d34a473bb4cd5915674a3214281d0d094909
Brian Pepple <bdpepple@gmail.com>
2019-05-30 15:18:07 -0400

    Add test for taggerlib setting. Closes #2

---
+++ tests/test_taggerlib_settings.py
@@ -0,0 +1,27 @@
+import tempfile
+import unittest
+
+from metrontagger.taggerlib.settings import MetronTaggerSettings
+
+
+class TestSettings(unittest.TestCase):
+    def setUp(self):
+        self.tmp_dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.tmp_dir.cleanup()
+
+    def test_settings(self):
+        user = "test"
+        passwd = "test_passwd"
+        # Save a test config file
+        config = MetronTaggerSettings(config_dir=self.tmp_dir.name)
+        config.metron_user = user
+        config.metron_pass = passwd
+        config.save()
+
+        # Now load that file and verify the contents
+        new_config = MetronTaggerSettings(config_dir=self.tmp_dir.name)
+        new_config.load()
+        self.assertEqual(new_config.metron_user, user)
+        self.assertEqual(new_config.metron_pass, passwd)
