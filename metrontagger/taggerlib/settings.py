04befec91cd1d0d64b8b673ee5f0c6d66b2a0440
Brian Pepple <bdpepple@gmail.com>
2019-05-30 14:46:42 -0400

    Refactor configparse to remove legacy api calls

--- metrontagger/taggerlib/settings.py
+++ metrontagger/taggerlib/settings.py
@@ -1,4 +1,3 @@
-import codecs
 import configparser
 import os
 import platform
@@ -31,7 +30,7 @@ class MetronTaggerSettings:
         if not os.path.exists(self.folder):
             os.makedirs(self.folder)
 
-        self.settings_file = os.path.join(self.folder, "settings")
+        self.settings_file = os.path.join(self.folder, "settings.ini")
 
         # Write the config file if it doesn't exist
         if not os.path.exists(self.settings_file):
@@ -43,17 +42,17 @@ class MetronTaggerSettings:
         self.config.read(self.settings_file)
 
         if self.config.has_option("metron", "user"):
-            self.metron_user = self.config.get("metron", "user")
+            self.metron_user = self.config["metron"]["user"]
 
         if self.config.has_option("metron", "password"):
-            self.metron_pass = self.config.get("metron", "password")
+            self.metron_pass = self.config["metron"]["password"]
 
     def save(self):
         if not self.config.has_section("metron"):
             self.config.add_section("metron")
 
-        self.config.set("metron", "user", self.metron_user)
-        self.config.set("metron", "password", self.metron_pass)
+        self.config["metron"]["user"] = self.metron_user
+        self.config["metron"]["password"] = self.metron_pass
 
-        with codecs.open(self.settings_file, "wb", "utf8") as configfile:
+        with open(self.settings_file, "w") as configfile:
             self.config.write(configfile)
