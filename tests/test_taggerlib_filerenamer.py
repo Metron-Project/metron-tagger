65a99003d85b3687cb4d2e6b80a45970a67c591f
Brian Pepple <bdpepple@gmail.com>
2019-06-04 11:34:56 -0400

    Use a zip tempfile for the filerenamer test

--- tests/test_taggerlib_filerenamer.py
+++ tests/test_taggerlib_filerenamer.py
@@ -1,4 +1,7 @@
+import os
+import tempfile
 import unittest
+from shutil import make_archive
 
 from metrontagger.comicapi.genericmetadata import GenericMetadata
 from metrontagger.taggerlib.filerenamer import FileRenamer
@@ -6,15 +9,41 @@ from metrontagger.taggerlib.filerenamer import FileRenamer
 
 class TestFileRenamer(unittest.TestCase):
     def setUp(self):
+        self.tmp_archive_dir = tempfile.TemporaryDirectory()
+        self.tmp_image_dir = tempfile.TemporaryDirectory()
+        # Create 3 fake jpgs
+        img_1 = tempfile.NamedTemporaryFile(
+            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
+        )
+        img_1.write(b"test data")
+        img_2 = tempfile.NamedTemporaryFile(
+            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
+        )
+        img_2.write(b"more data")
+        img_3 = tempfile.NamedTemporaryFile(
+            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
+        )
+        img_3.write(b"yet more data")
+
+        self.zipfile = os.path.join(self.tmp_archive_dir.name, "comic")
+
+        # Create zipfile
+        open(make_archive(self.zipfile, "zip", self.tmp_image_dir.name), "rb").read()
+
+        # Create test metadata
         self.md = GenericMetadata()
         self.md.series = "Aquaman"
         self.md.issue = "1"
         self.md.year = "2011"
 
+    def tearDown(self):
+        self.tmp_archive_dir.cleanup()
+        self.tmp_image_dir.cleanup()
+
     def test_determine_name(self):
         renamer = FileRenamer(self.md)
-        new_name = renamer.determineName("/tmp/test.cbz")
-        self.assertEqual(new_name, "Aquaman #001 (2011).cbz")
+        new_name = renamer.determineName(self.zipfile + ".zip")
+        self.assertEqual(new_name, "Aquaman #001 (2011).zip")
 
 
 if __name__ == "__main__":
