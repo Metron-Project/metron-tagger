bd04fb4396bec5653ae235043f484fce74915edb
Brian Pepple <bdpepple@gmail.com>
2019-07-14 17:18:13 -0400

    Write a test for the create_issue_query_dict()

--- tests/test_taggerlib_utils.py
+++ tests/test_taggerlib_utils.py
@@ -1,9 +1,18 @@
+import tempfile
 import unittest
 
-from metrontagger.taggerlib.utils import cleanup_string
+from metrontagger.taggerlib.utils import cleanup_string, create_issue_query_dict
 
 
 class TestTaggerlibUtils(unittest.TestCase):
+    def setUp(self):
+        self.series = "Aquaman"
+        self.number = "1"
+        self.year = "1962"
+        self.tmp_file = tempfile.NamedTemporaryFile(
+            prefix=f"{self.series} #{self.number} ({self.year})", suffix=".cbz"
+        )
+
     def test_cleanup_colon_space(self):
         test_str = "Hashtag: Danger (2019)"
         res = cleanup_string(test_str)
@@ -28,6 +37,16 @@ class TestTaggerlibUtils(unittest.TestCase):
         test_str = "What If? (2019)"
         res = cleanup_string(test_str)
         self.assertEqual(res, "What If (2019)")
+
+    def test_create_issue_query_dict(self):
+        i = create_issue_query_dict(self.tmp_file.name)
+        results = {
+            "series": f"{self.series}",
+            "volume": "",
+            "number": f"{self.number}",
+            "year": f"{self.year}",
+        }
+        self.assertEqual(i, results)
 
 
 if __name__ == "__main__":
