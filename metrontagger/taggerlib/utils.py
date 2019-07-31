9c35ad6c007cbf9c3caf77ea789d69717cf4ed14
Brian Pepple <bdpepple@gmail.com>
2019-07-31 15:43:03 -0400

    Handle issue numbers like '½' correctly

--- metrontagger/taggerlib/utils.py
+++ metrontagger/taggerlib/utils.py
@@ -22,10 +22,11 @@ def create_issue_query_dict(filename):
     series_word_list = fixed_txt.split()
     series_string = " ".join(series_word_list).strip()
     series_string = urllib.parse.quote_plus(series_string.encode("utf-8"))
+    number = urllib.parse.quote_plus(fnp.issue.encode("utf-8"))
     query_dict = {
         "series": series_string,
         "volume": fnp.volume,
-        "number": fnp.issue,
+        "number": number,
         "year": fnp.year,
     }
 
