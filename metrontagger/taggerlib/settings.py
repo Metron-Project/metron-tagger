dc26773f57f63104eb42c7f3d2ba7cab335b84c1
Brian Pepple <bdpepple@gmail.com>
2019-11-11 17:07:48 -0500

    Fix some pylint warnings in settings.py

--- metrontagger/taggerlib/settings.py
+++ metrontagger/taggerlib/settings.py
@@ -1,11 +1,14 @@
+"""Class to handle project settings"""
 import configparser
 import os
 import platform
 
 
 class MetronTaggerSettings:
+    """Class to handle project settings"""
     @staticmethod
-    def getSettingsFolder():
+    def get_settings_folder():
+        """Method to determine where the users settings should be saved"""
         if platform.system() == "Windows":
             folder = os.path.join(os.environ["APPDATA"], "MetronTagger")
         else:
@@ -14,7 +17,8 @@ class MetronTaggerSettings:
             folder = folder
         return folder
 
-    def setDefaultValues(self):
+    def set_default_values(self):
+        """Method to set default values as empty"""
         # Metron creditials
         self.metron_user = ""
         self.metron_pass = ""
@@ -23,10 +27,10 @@ class MetronTaggerSettings:
     def __init__(self, config_dir=None):
         self.settings_file = ""
         self.folder = ""
-        self.setDefaultValues()
+        self.set_default_values()
 
         self.config = configparser.ConfigParser()
-        self.folder = config_dir or MetronTaggerSettings.getSettingsFolder()
+        self.folder = config_dir or MetronTaggerSettings.get_settings_folder()
 
         if not os.path.exists(self.folder):
             os.makedirs(self.folder)
@@ -40,6 +44,7 @@ class MetronTaggerSettings:
             self.load()
 
     def load(self):
+        """Method to retrieve a users settings"""
         self.config.read(self.settings_file)
 
         if self.config.has_option("metron", "user"):
@@ -52,6 +57,7 @@ class MetronTaggerSettings:
             self.sort_dir = self.config["DEFAULT"]["sort_dir"]
 
     def save(self):
+        """Method to save a users settings"""
         if not self.config.has_section("metron"):
             self.config.add_section("metron")
 
