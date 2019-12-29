7f741f6c4ffcce45c3e586b8dde03bdd111ac0fb
Brian Pepple <bdpepple@gmail.com>
2019-12-29 16:39:34 -0500

    Use relative imports and fix to lint warnings

--- metrontagger/tests/test_comicapi_comicinfoxml.py
+++ metrontagger/tests/test_comicapi_comicinfoxml.py
@@ -1,33 +1,38 @@
+""" Tests for ComicInfo Tags """
 import tempfile
 from unittest import TestCase, main
 
-from metrontagger.comicapi.comicinfoxml import ComicInfoXml
-from metrontagger.comicapi.genericmetadata import GenericMetadata
+from ..comicapi.comicinfoxml import ComicInfoXml
+from ..comicapi.genericmetadata import GenericMetadata
 
 
 class TestComicInfoXml(TestCase):
+    """ Collection of tests for ComicInfo tags """
+
     def setUp(self):
-        self.md = GenericMetadata()
-        self.md.series = "Aquaman"
-        self.md.issue = "1"
-        self.md.year = "1993"
-        self.md.day = "15"
-        self.md.addCredit("Peter David", "Writer", primary=True)
-        self.md.addCredit("Martin Egeland", "Penciller")
-        self.md.addCredit("Martin Egeland", "Cover")
-        self.md.addCredit("Kevin Dooley", "Editor")
-        self.md.addCredit("Howard Shum", "Inker")
-        self.md.addCredit("Tom McCraw", "Colorist")
-        self.md.addCredit("Dan Nakrosis", "Letterer")
+        self.meta_data = GenericMetadata()
+        self.meta_data.series = "Aquaman"
+        self.meta_data.issue = "1"
+        self.meta_data.year = "1993"
+        self.meta_data.day = "15"
+        self.meta_data.addCredit("Peter David", "Writer", primary=True)
+        self.meta_data.addCredit("Martin Egeland", "Penciller")
+        self.meta_data.addCredit("Martin Egeland", "Cover")
+        self.meta_data.addCredit("Kevin Dooley", "Editor")
+        self.meta_data.addCredit("Howard Shum", "Inker")
+        self.meta_data.addCredit("Tom McCraw", "Colorist")
+        self.meta_data.addCredit("Dan Nakrosis", "Letterer")
 
     def test_metadata_from_xml(self):
-        res = ComicInfoXml().stringFromMetadata(self.md)
+        """ Simple test of creating the ComicInfo """
+        res = ComicInfoXml().stringFromMetadata(self.meta_data)
         # TODO: add more asserts to verify data.
         self.assertIsNotNone(res)
 
     def test_meta_write_to_file(self):
+        """ Test of writing the metadata to a file """
         tmp_file = tempfile.NamedTemporaryFile(suffix=".xml")
-        ComicInfoXml().writeToExternalFile(tmp_file.name, self.md)
+        ComicInfoXml().writeToExternalFile(tmp_file.name, self.meta_data)
         # Read the contents of the file just written.
         # TODO: Verify the data.
         res = open(tmp_file.name).read()
@@ -35,19 +40,20 @@ class TestComicInfoXml(TestCase):
         self.assertIsNotNone(res)
 
     def test_read_from_file(self):
+        """ Test to read in the data from a file """
         tmp_file = tempfile.NamedTemporaryFile(suffix=".xml")
         # Write metadata to file
-        ComicInfoXml().writeToExternalFile(tmp_file.name, self.md)
+        ComicInfoXml().writeToExternalFile(tmp_file.name, self.meta_data)
         # Read the metadat from the file
         new_md = ComicInfoXml().readFromExternalFile(tmp_file.name)
         tmp_file.close()
 
         self.assertIsNotNone(new_md)
-        self.assertEqual(new_md.series, self.md.series)
-        self.assertEqual(new_md.issue, self.md.issue)
-        self.assertEqual(new_md.year, self.md.year)
-        self.assertEqual(new_md.month, self.md.month)
-        self.assertEqual(new_md.day, self.md.day)
+        self.assertEqual(new_md.series, self.meta_data.series)
+        self.assertEqual(new_md.issue, self.meta_data.issue)
+        self.assertEqual(new_md.year, self.meta_data.year)
+        self.assertEqual(new_md.month, self.meta_data.month)
+        self.assertEqual(new_md.day, self.meta_data.day)
 
 
 if __name__ == "__main__":
