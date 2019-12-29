4bce7e2e5d60f9815ed16f102bb9d03396516404
Brian Pepple <bdpepple@gmail.com>
2019-12-29 16:47:37 -0500

    Use relative import and rename some variables

--- metrontagger/tests/test_comicapi_genericmetadata.py
+++ metrontagger/tests/test_comicapi_genericmetadata.py
@@ -1,31 +1,31 @@
 from unittest import TestCase, main
 
-from metrontagger.comicapi.genericmetadata import GenericMetadata
+from ..comicapi.genericmetadata import GenericMetadata
 
 
 class TestGenericMetadata(TestCase):
     def setUp(self):
-        self.md = GenericMetadata()
-        self.md.series = "Aquaman"
-        self.md.issue = "0"
-        self.md.title = "A Crash of Symbols"
-        self.md.isEmpty = False
+        self.meta_data = GenericMetadata()
+        self.meta_data.series = "Aquaman"
+        self.meta_data.issue = "0"
+        self.meta_data.title = "A Crash of Symbols"
+        self.meta_data.isEmpty = False
 
         self.new_md = GenericMetadata()
         self.new_md.year = "1994"
         self.new_md.month = "10"
         self.new_md.day = "1"
-        self.md.isEmpty = False
+        self.meta_data.isEmpty = False
 
     def test_metadata_overlay(self):
-        self.md.overlay(self.new_md)
+        self.meta_data.overlay(self.new_md)
 
-        self.assertEqual(self.md.series, "Aquaman")
-        self.assertEqual(self.md.issue, "0")
-        self.assertEqual(self.md.title, "A Crash of Symbols")
-        self.assertEqual(self.md.year, "1994")
-        self.assertEqual(self.md.month, "10")
-        self.assertEqual(self.md.day, "1")
+        self.assertEqual(self.meta_data.series, "Aquaman")
+        self.assertEqual(self.meta_data.issue, "0")
+        self.assertEqual(self.meta_data.title, "A Crash of Symbols")
+        self.assertEqual(self.meta_data.year, "1994")
+        self.assertEqual(self.meta_data.month, "10")
+        self.assertEqual(self.meta_data.day, "1")
 
     def test_metadata_credits(self):
         result = [
@@ -34,11 +34,11 @@ class TestGenericMetadata(TestCase):
             {"person": "Martin Egeland", "role": "Cover"},
         ]
 
-        self.md.addCredit("Peter David", "Writer", primary=True)
-        self.md.addCredit("Martin Egeland", "Penciller")
-        self.md.addCredit("Martin Egeland", "Cover")
+        self.meta_data.addCredit("Peter David", "Writer", primary=True)
+        self.meta_data.addCredit("Martin Egeland", "Penciller")
+        self.meta_data.addCredit("Martin Egeland", "Cover")
 
-        self.assertEqual(self.md.credits, result)
+        self.assertEqual(self.meta_data.credits, result)
 
     def test_metadata_credits_overlay(self):
         new_credit = [{"person": "Tom McCray", "role": "Colorist"}]
@@ -47,19 +47,19 @@ class TestGenericMetadata(TestCase):
             {"person": "Tom McCray", "role": "Colorist"},
         ]
 
-        self.md.addCredit("Peter David", "Writer")
-        self.md.overlayCredits(new_credit)
+        self.meta_data.addCredit("Peter David", "Writer")
+        self.meta_data.overlayCredits(new_credit)
 
-        self.assertEqual(self.md.credits, result)
+        self.assertEqual(self.meta_data.credits, result)
 
     def test_metadata_print_str(self):
         self.assertEqual(
-            str(self.md), "series: Aquaman\nissue:  0\ntitle:  A Crash of Symbols\n"
+            str(self.meta_data), "series: Aquaman\nissue:  0\ntitle:  A Crash of Symbols\n"
         )
 
     def test_no_metadata_print_str(self):
-        md = GenericMetadata()
-        self.assertEqual(str(md), "No metadata")
+        m_data = GenericMetadata()
+        self.assertEqual(str(m_data), "No metadata")
 
 
 if __name__ == "__main__":
