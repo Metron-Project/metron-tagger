ffe681d25ca3c8260c772e82a64d015af8e6358d
Brian Pepple <bdpepple@gmail.com>
2020-02-01 16:18:31 -0500

    Rewrite some tests to use pytest

--- tests/test_taggerlib_settings.py
+++ tests/test_taggerlib_settings.py
@@ -1,30 +1,16 @@
-import tempfile
-from unittest import TestCase, main
-
 from metrontagger.taggerlib.settings import MetronTaggerSettings
 
 
-class TestSettings(TestCase):
-    def setUp(self):
-        self.tmp_dir = tempfile.TemporaryDirectory()
+def test_read_settings(tmpdir):
+    user = "test"
+    dummy = "dummy_value"
 
-    def tearDown(self):
-        self.tmp_dir.cleanup()
+    config = MetronTaggerSettings(config_dir=tmpdir)
+    config.metron_user = user
+    config.metron_pass = dummy
+    config.save()
 
-    def test_settings(self):
-        user = "test"
-        dummy = "dummy_value"
-        # Save a test config file
-        config = MetronTaggerSettings(config_dir=self.tmp_dir.name)
-        config.metron_user = user
-        config.metron_pass = dummy
-        config.save()
-
-        # Now load that file and verify the contents
-        new_config = MetronTaggerSettings(config_dir=self.tmp_dir.name)
-        self.assertEqual(new_config.metron_user, user)
-        self.assertEqual(new_config.metron_pass, dummy)
-
-
-if __name__ == "__main__":
-    main()
+    # Now load that file and verify the contents
+    new_config = MetronTaggerSettings(config_dir=tmpdir)
+    assert new_config.metron_user == user
+    assert new_config.metron_pass == dummy
