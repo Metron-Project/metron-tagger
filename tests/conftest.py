e87ce82123a3c14c6e42381c76aa2e9029c7da25
Brian Pepple <bdpepple@gmail.com>
2020-02-11 11:42:38 -0500

    Make a fixture for the MetronTalker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
+from base64 import standard_b64encode
+
 import pytest
 
+from metrontagger.taggerlib.metrontalker import MetronTalker
 from metrontagger.taggerlib.options import make_parser
 
 
@@ -7,3 +10,11 @@ from metrontagger.taggerlib.options import make_parser
 def parser():
     parser = make_parser()
     return parser
+
+
+@pytest.fixture(scope="module")
+def talker():
+    auth = f"test_user:test_auth"
+    base64string = standard_b64encode(auth.encode("utf-8"))
+    talker = MetronTalker(base64string)
+    return talker
