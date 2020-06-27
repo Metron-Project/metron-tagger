c130818b1cb39c0c732bc4c21512aa885d744fb4
Brian Pepple <bdpepple@gmail.com>
2020-06-27 17:14:35 -0400

    Refactor FileRename tests to use pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from base64 import standard_b64encode
+from shutil import make_archive
 
 import pytest
 
@@ -18,3 +19,23 @@ def talker():
     base64string = standard_b64encode(auth.encode("utf-8"))
     talker = MetronTalker(base64string)
     return talker
+
+
+@pytest.fixture(scope="module")
+def fake_comic(tmpdir_factory):
+    image_dir = tmpdir_factory.mktemp("images")
+
+    img_1 = image_dir.join("image-1.jpg")
+    img_1.write(b"test data")
+
+    img_2 = image_dir.join("image-2.jpg")
+    img_2.write(b"more data")
+
+    img_3 = image_dir.join("image-3.jpg")
+    img_3.write(b"yet more data")
+
+    comic_dir = tmpdir_factory.mktemp("comic")
+
+    open(make_archive(comic_dir, "zip", image_dir))
+
+    return comic_dir + ".zip"
--- tests/test_taggerlib_filerenamer.py
+++ tests/test_taggerlib_filerenamer.py
@@ -1,50 +1,16 @@
-import os
-import tempfile
-from shutil import make_archive
-from unittest import TestCase, main
-
 from darkseid.genericmetadata import GenericMetadata
 from metrontagger.taggerlib.filerenamer import FileRenamer
 
 
-class TestFileRenamer(TestCase):
-    def setUp(self):
-        self.tmp_archive_dir = tempfile.TemporaryDirectory()
-        self.tmp_image_dir = tempfile.TemporaryDirectory()
-        # Create 3 fake jpgs
-        img_1 = tempfile.NamedTemporaryFile(
-            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
-        )
-        img_1.write(b"test data")
-        img_2 = tempfile.NamedTemporaryFile(
-            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
-        )
-        img_2.write(b"more data")
-        img_3 = tempfile.NamedTemporaryFile(
-            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
-        )
-        img_3.write(b"yet more data")
+def test_determine_name(fake_comic):
+    meta_data = GenericMetadata()
+    meta_data.series = "Aquaman"
+    meta_data.volume = "2"
+    meta_data.issue = "1"
+    meta_data.year = "2011"
 
-        self.zipfile = os.path.join(self.tmp_archive_dir.name, "comic")
+    expected_result = "Aquaman v2 #001 (2011).zip"
 
-        # Create zipfile
-        open(make_archive(self.zipfile, "zip", self.tmp_image_dir.name), "rb").read()
-
-        # Create test metadata
-        self.meta_data = GenericMetadata()
-        self.meta_data.series = "Aquaman"
-        self.meta_data.issue = "1"
-        self.meta_data.year = "2011"
-
-    def tearDown(self):
-        self.tmp_archive_dir.cleanup()
-        self.tmp_image_dir.cleanup()
-
-    def test_determine_name(self):
-        renamer = FileRenamer(self.meta_data)
-        new_name = renamer.determine_name(self.zipfile + ".zip")
-        self.assertEqual(new_name, "Aquaman #001 (2011).zip")
-
-
-if __name__ == "__main__":
-    main()
+    renamer = FileRenamer(meta_data)
+    new_file_name = renamer.determine_name(fake_comic)
+    assert new_file_name == expected_result
