8209837a696b28a23144a2b2502515be96894c1d
Brian Pepple <bdpepple@gmail.com>
2020-06-27 17:48:07 -0400

    Move fake metadata to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ from base64 import standard_b64encode
 from shutil import make_archive
 
 import pytest
+from darkseid.genericmetadata import GenericMetadata
 
 from metrontagger.taggerlib.metrontalker import MetronTalker
 from metrontagger.taggerlib.options import make_parser
@@ -19,6 +20,18 @@ def talker():
     base64string = standard_b64encode(auth.encode("utf-8"))
     talker = MetronTalker(base64string)
     return talker
+
+
+@pytest.fixture(scope="session")
+def fake_metadata():
+    meta_data = GenericMetadata()
+    meta_data.publisher = "DC Comics"
+    meta_data.series = "Aquaman"
+    meta_data.volume = "2"
+    meta_data.issue = "1"
+    meta_data.year = "2011"
+
+    return meta_data
 
 
 @pytest.fixture(scope="module")
--- tests/test_taggerlib_filerenamer.py
+++ tests/test_taggerlib_filerenamer.py
@@ -1,16 +1,9 @@
-from darkseid.genericmetadata import GenericMetadata
 from metrontagger.taggerlib.filerenamer import FileRenamer
 
 
-def test_determine_name(fake_comic):
-    meta_data = GenericMetadata()
-    meta_data.series = "Aquaman"
-    meta_data.volume = "2"
-    meta_data.issue = "1"
-    meta_data.year = "2011"
-
+def test_determine_name(fake_comic, fake_metadata):
     expected_result = "Aquaman v2 #001 (2011).zip"
 
-    renamer = FileRenamer(meta_data)
+    renamer = FileRenamer(fake_metadata)
     new_file_name = renamer.determine_name(fake_comic)
     assert new_file_name == expected_result
