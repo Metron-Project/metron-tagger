989b404d070a38c8d84accc0a87e759925c8be6f
Brian Pepple <bdpepple@gmail.com>
2020-07-09 08:28:21 -0400

    Refactor some more tests

--- tests/test_taggerlib_filesorter.py
+++ tests/test_taggerlib_filesorter.py
@@ -1,75 +1,55 @@
-import os
-import tempfile
+import zipfile
 from pathlib import Path
-from shutil import make_archive
-from unittest import TestCase, main
 
+import pytest
 from darkseid.comicarchive import ComicArchive
-from darkseid.genericmetadata import GenericMetadata
 
 from metrontagger.taggerlib.filesorter import FileSorter
 
-
-class TestFileSorter(TestCase):
-    def setUp(self):
-        self.tmp_archive_dir = tempfile.TemporaryDirectory()
-        self.tmp_image_dir = tempfile.TemporaryDirectory()
-        self.tmp_sort_dir = tempfile.TemporaryDirectory()
-        # Create 3 fake jpgs
-        img_1 = tempfile.NamedTemporaryFile(
-            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
-        )
-        img_1.write(b"test data")
-        img_2 = tempfile.NamedTemporaryFile(
-            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
-        )
-        img_2.write(b"more data")
-        img_3 = tempfile.NamedTemporaryFile(
-            suffix=".jpg", dir=self.tmp_image_dir.name, mode="wb"
-        )
-        img_3.write(b"yet more data")
-
-        self.zfile = os.path.join(self.tmp_archive_dir.name, "comic")
-
-        # Create zipfile
-        open(make_archive(self.zfile, "zip", self.tmp_image_dir.name), "rb").read()
-
-        # Create test metadata
-        self.meta_data = GenericMetadata()
-        self.meta_data.series = "Aquaman"
-        self.meta_data.volume = "1"
-        self.meta_data.issue = "1"
-        self.meta_data.year = "2011"
-        self.meta_data.publisher = "DC Comics"
-
-        # Now write it to the zipfile
-        comic_archive = ComicArchive(self.zfile + ".zip")
-        comic_archive.write_metadata(self.meta_data)
-
-    def tearDown(self):
-        self.tmp_archive_dir.cleanup()
-        self.tmp_sort_dir.cleanup()
-        self.tmp_image_dir.cleanup()
-
-    def test_sort_file(self):
-        result_dir = Path(self.tmp_sort_dir.name).joinpath(
-            self.meta_data.publisher, self.meta_data.series, f"v{self.meta_data.volume}"
-        )
-        file_sorter = FileSorter(self.tmp_sort_dir.name)
-        res = file_sorter.sort_comics(self.zfile + ".zip")
-        self.assertTrue(result_dir.is_dir())
-        self.assertTrue(res)
-
-    def test_sort_files_without_metadata(self):
-        # If we add more tests we should probably create another tmpfile
-        # since we are removing the metadata from the tmpfile
-        comic = ComicArchive(self.zfile + ".zip")
-        comic.remove_metadata()
-
-        file_sorter = FileSorter(self.tmp_sort_dir.name)
-        res = file_sorter.sort_comics(self.zfile + ".zip")
-        self.assertFalse(res)
+CONTENT = "blah blah blah"
 
 
-if __name__ == "__main__":
-    main()
+@pytest.fixture()
+def fake_comic(tmp_path, fake_metadata):
+    img_1 = tmp_path / "image-1.jpg"
+    img_1.write_text(CONTENT)
+    img_2 = tmp_path / "image-2.jpg"
+    img_2.write_text(CONTENT)
+    img_3 = tmp_path / "image-3.jpg"
+    img_3.write_text(CONTENT)
+
+    z_file = tmp_path / "Aquaman v1 #001 (of 08) (1994).cbz"
+    zf = zipfile.ZipFile(z_file, "w")
+    try:
+        zf.write(img_1)
+        zf.write(img_2)
+        zf.write(img_3)
+    finally:
+        zf.close()
+
+    comic = ComicArchive(z_file)
+    comic.write_metadata(fake_metadata)
+
+    return z_file
+
+
+def test_sort_comic(fake_comic, fake_metadata, tmpdir):
+    test_dir = tmpdir
+    result_dir = Path(test_dir).joinpath(
+        fake_metadata.publisher, fake_metadata.series, f"v{fake_metadata.volume}"
+    )
+    file_sorter = FileSorter(test_dir)
+    res = file_sorter.sort_comics(fake_comic)
+    assert result_dir.is_dir()
+    assert res is True
+
+
+def test_sort_files_without_metadata(fake_comic, tmpdir):
+    # If we add more tests we should probably create another tmpfile
+    # since we are removing the metadata from the tmpfile
+    comic = ComicArchive(fake_comic)
+    comic.remove_metadata()
+
+    file_sorter = FileSorter(tmpdir)
+    res = file_sorter.sort_comics(fake_comic)
+    assert res is False
