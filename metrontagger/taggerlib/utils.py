2ac69a9e025b84cf3f7d19ad6a054d6d6c702cfc
Brian Pepple <bdpepple@gmail.com>
2020-08-18 22:12:22 -0400

    Use type hinting

--- metrontagger/taggerlib/utils.py
+++ metrontagger/taggerlib/utils.py
@@ -1,10 +1,11 @@
 """Some miscellaneous functions"""
+from typing import Dict, List
 from urllib.parse import quote_plus
 
 from darkseid.filenameparser import FileNameParser
 
 
-def cleanup_string(path_name):
+def cleanup_string(path_name: str) -> str:
     """Function to remove some characters that don't play nicely on Windows machines filesystem"""
     path_name = path_name.replace("/", "-")
     path_name = path_name.replace(" :", " -")
@@ -15,18 +16,18 @@ def cleanup_string(path_name):
     return path_name
 
 
-def create_issue_query_dict(filename):
+def create_issue_query_dict(filename: str) -> Dict[str, str]:
     """Function to create a diction of values based on the provided filename"""
     fnp = FileNameParser()
     fnp.parse_filename(filename)
 
     # Substitute colon for hyphen when searching for series name
-    fixed_txt = fnp.series.replace(" - ", ": ")
-    series_word_list = fixed_txt.split()
-    series_string = " ".join(series_word_list).strip()
+    fixed_txt: str = fnp.series.replace(" - ", ": ")
+    series_word_list: List[str] = fixed_txt.split()
+    series_string: str = " ".join(series_word_list).strip()
     series_string = quote_plus(series_string.encode("utf-8"))
     # If there isn't an issue number, let's assume it's "1".
-    number = quote_plus(fnp.issue.encode("utf-8")) if fnp.issue else "1"
+    number: str = quote_plus(fnp.issue.encode("utf-8")) if fnp.issue else "1"
     return {
         "series": series_string,
         "volume": fnp.volume,
