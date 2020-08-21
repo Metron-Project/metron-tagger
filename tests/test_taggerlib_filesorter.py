99359dbf983880d67a034f1c23a2966e93830493
Brian Pepple <bdpepple@gmail.com>
2020-08-21 11:51:40 -0400

    Add type hinting to filesorter
    Reworked some code due to possible errors exposed by type hinting

--- tests/test_taggerlib_filesorter.py
+++ tests/test_taggerlib_filesorter.py
@@ -3,10 +3,23 @@ from darkseid.comicarchive import ComicArchive
 from metrontagger.taggerlib.filesorter import FileSorter
 
 
+def test_sort_comic_with_missing_metadata(fake_comic, fake_metadata, tmp_path):
+    test_dir = tmp_path / "sort10"
+
+    fake_metadata.volume = None
+
+    comic = ComicArchive(fake_comic)
+    if comic.has_metadata():
+        comic.remove_metadata()
+    comic.write_metadata(fake_metadata)
+    file_sorter = FileSorter(test_dir)
+    res = file_sorter.sort_comics(fake_comic)
+    assert res is False
+
+
 def test_sort_comic(fake_comic, fake_metadata, tmp_path):
     test_dir = tmp_path / "sort1"
-    test_dir.mkdir()
-    
+
     result_dir = (
         test_dir
         / fake_metadata.publisher
@@ -16,6 +29,8 @@ def test_sort_comic(fake_comic, fake_metadata, tmp_path):
 
     # Write metadata to fake file
     comic = ComicArchive(fake_comic)
+    if comic.has_metadata():
+        comic.remove_metadata()
     comic.write_metadata(fake_metadata)
 
     file_sorter = FileSorter(test_dir)
@@ -26,7 +41,6 @@ def test_sort_comic(fake_comic, fake_metadata, tmp_path):
 
 def test_sort_files_without_metadata(fake_comic, tmp_path):
     test_dir = tmp_path / "sort2"
-    test_dir.mkdir()
     # If we add more tests we should probably create another tmpfile
     # since we are removing the metadata from the tmpfile
     comic = ComicArchive(fake_comic)
