aab74133ec7d3bd32df7bdad95070ba1553ce0e0
Brian Pepple <bdpepple@gmail.com>
2020-09-08 14:55:43 -0400

    Bump minimum version of darkseid needed

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license="GPLv3",
     packages=find_packages(exclude=["tests"]),
     python_requires=">=3.6",
-    install_requires=["darkseid >= 0.7.0", "ratelimit"],
+    install_requires=["darkseid >= 1.0.3", "ratelimit"],
     entry_points={"console_scripts": ["metron-tagger=metrontagger.main:main"]},
     classifiers=[
         "Development Status :: 4 - Beta",
