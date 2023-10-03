927de7759df0a6ac144de5a6042e063da81b4608
Brian Pepple <bdpepple@gmail.com>
2023-10-03 14:58:08 -0400

    Improve handling of series format better
    Use abbreviated words for 'Hard Cover' & 'Trade Paperback'

--- tests/test_filerenamer.py
+++ tests/test_filerenamer.py
@@ -33,6 +33,28 @@ def test_rename_file(fake_comic: ZipFile) -> None:
     assert expected_result == renamed_file.name
 
 
+def test_rename_tpb(fake_tpb: ZipFile, fake_tpb_metadata: Metadata) -> None:
+    fn = (
+        "Batman - The Adventures Continue Season One (2021) "
+        "(digital) (Son of Ultron-Empire).cbz"
+    )
+    assert fake_tpb.name == fn
+    renamer = FileRenamer(fake_tpb_metadata)
+    renamer.set_template("%series% %format% v%volume% #%issue% (%year%)")
+    renamer.set_issue_zero_padding(3)
+    renamer.set_smart_cleanup(True)
+    renamed_file = renamer.rename_file(fake_tpb)
+    assert renamed_file is not None
+    expected_result = (
+        f"{fake_tpb_metadata.series.name} "
+        "TPB "
+        f"v{fake_tpb_metadata.series.volume} "
+        f"#00{fake_tpb_metadata.issue} "
+        f"({fake_tpb_metadata.cover_date.year}).cbz"
+    )
+    assert expected_result == renamed_file.name
+
+
 def test_half_issues_rename(fake_comic: ZipFile) -> None:
     md = Metadata()
     md.series = Series("Batman", volume=2)
