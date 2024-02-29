e1d1ed85b767331245a243b28436dc36043b8d9e
Brian Pepple <bdpepple@gmail.com>
2024-02-29 10:33:41 -0500

    comicfn2dict (#97)
    * Add dep on `comicfn2dict`
    * Update import for new version of mokkari
    * Use `comicfn2dict` to create issue params

--- metrontagger/utils.py
+++ metrontagger/utils.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from urllib.parse import quote_plus
 
-from darkseid.filename import FileNameParser
+from comicfn2dict import comicfn2dict
 
 
 def cleanup_string(path_name: str | None) -> str | None:
@@ -24,27 +24,31 @@ def cleanup_string(path_name: str | None) -> str | None:
 
 def create_query_params(filename: Path) -> dict[str, str]:
     """Function to create a diction of values based on the provided filename"""
-    fnp = FileNameParser()
-    fnp.parse_filename(filename)
+    metadata: dict[str, str | tuple[str, ...]] = comicfn2dict(filename, verbose=0)
 
+    # TODO: Should probably check if there is a 'series' key.
     # Remove hyphen when searching for series name
-    fixed_txt: str = fnp.series.replace(" - ", " ")
-    series_word_list: list[str] = fixed_txt.split()
-    series_string: str = " ".join(series_word_list).strip()
+    series_string: str = metadata["series"].replace(" - ", " ").strip()
 
     # If there isn't an issue number, let's assume it's "1".
-    number: str = quote_plus(fnp.issue.encode("utf-8")) if fnp.issue else "1"
+    number: str = quote_plus(metadata["issue"].encode("utf-8")) if "issue" in metadata else "1"
+
+    # Strip any leading zeros from the issue number for the API to correctly match.
+    number = number.lstrip("0")
+
     # Handle issues with #½
-    if number == "0.5":
+    if number == ".5":
         number = "½"
 
     params = {
         "series_name": series_string,
         "number": number,
     }
-    if fnp.volume:
-        params["series_volume"] = fnp.volume
-    if fnp.year:
-        params["cover_year"] = fnp.year
+    # TODO: Probably want to remove these from the API call and use the info instead to find the issue
+    #       from the return results.
+    if "volume" in metadata:
+        params["series_volume"] = metadata["volume"]
+    if "year" in metadata:
+        params["cover_year"] = metadata["year"]
 
     return params
