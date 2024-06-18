d23b8d9f966d47419ed62f904578a15fed187375
Brian Pepple <bdpepple@gmail.com>
2024-06-18 16:34:05 -0400

    Add docstrings to styles.py

--- metrontagger/styles.py
+++ metrontagger/styles.py
@@ -1,4 +1,9 @@
 class Styles:
+    """Class defining styles for console output.
+
+    This class provides predefined styles for different types of console output messages.
+    """
+
     TITLE = "bold fg:ansiblue"
     SUCCESS = "fg:ansigreen"
     WARNING = "fg:ansiyellow"
